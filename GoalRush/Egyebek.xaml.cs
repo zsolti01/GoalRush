623707139ff10495e5353ec70e6f87af0d3c593f
VecseiZsolt <vecseizs@kkszki.hu>
2025-05-17 11:06:39 +0200

    New windows added.

---
+++ GoalRush/Egyebek.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GoalRush
+{
+    /// <summary>
+    /// Interaction logic for Egyebek.xaml
+    /// </summary>
+    public partial class Egyebek : Window
+    {
+        public Egyebek()
+        {
+            InitializeComponent();
+        }
+    }
+}
